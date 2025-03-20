ebb2510046a044ebfcdde91ccbaf10ea0f8e2f95
Hiroto-Shida <uecshida6295@gmail.com>
2025-03-20 18:41:26 +0900

    :+1: 定数や関数の微修正

--- src/constants/omikuji.ts
+++ src/constants/omikuji.ts
@@ -1,5 +1,5 @@
 /**
- * おみくじの確率
+ * モアくじの確率
  */
 export const OMIKUJI = {
   大モア吉: 0.05,
@@ -11,7 +11,18 @@ export const OMIKUJI = {
   大モア凶: 0.05,
 } as const;
 
-export const OMIKUJI_LIST = Object.entries(OMIKUJI) as [
+// TODO: できれば OMIKUJI から OMIKUJI_ENUM を生成したい
+export const OMIKUJI_ENUM = [
+  "大モア吉",
+  "モア吉",
+  "中モア吉",
+  "小モア吉",
+  "末モア吉",
+  "モア凶",
+  "大モア凶",
+] as const;
+
+export const OMIKUJI_LIST = Object.entries(OMIKUJI) as readonly [
   keyof typeof OMIKUJI,
   number
 ][];
